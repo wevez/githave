c3ae5e6e428d2a122760083eb7d38ed1b85b470b
kometarou1145 <97835611+kometarou1145@users.noreply.github.com>
2024-04-20 06:20:11 +0900

    vanilla blink

--- app/src/main/java/githave/module/impl/player/Blink.java
+++ app/src/main/java/githave/module/impl/player/Blink.java
@@ -1,62 +1,84 @@
 package githave.module.impl.player;
 
 import githave.event.Events;
+import githave.gui.font.TTFFontRenderer;
 import githave.module.Module;
 import githave.module.ModuleCategory;
 import githave.module.setting.impl.BooleanSetting;
 import githave.module.setting.impl.DoubleSetting;
+import githave.module.setting.impl.MultiBooleanSetting;
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.network.INetHandler;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.INetHandlerPlayServer;
-import net.minecraft.network.play.client.C03PacketPlayer;
+import net.minecraft.network.play.client.*;
+import net.minecraft.util.ChatComponentText;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import tv.twitch.chat.Chat;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Blink extends Module {
+    private TTFFontRenderer font;
+
     private final BooleanSetting autoDisable = new BooleanSetting.Builder("Auto Disable")
             .value(false)
             .build();
 
-    private final DoubleSetting length = new DoubleSetting.Builder("Length", 100, 1, 5000, 1)
+    private final BooleanSetting autoReset = new BooleanSetting.Builder("Auto Reset")
+            .value(false)
             .build();
 
-    private final List<C03PacketPlayer> packetList = new ArrayList<>();
+    private final DoubleSetting length = new DoubleSetting.Builder("Length", 100, 1, 1000, 1)
+            .build();
+
+    private final MultiBooleanSetting packets = new MultiBooleanSetting.Builder("Cancel",
+            "C03Player", "C04Pos", "C05Look", "C06PosLook", "C07Digging", "C08Place", "C09ItemChange", "C13Abilities")
+            .value("C03Player", true)
+            .build();
+
+    private final List<Packet<?>> packetList = new ArrayList<>();
 
     public Blink() {
         super("Blink", "Make teleport like a lag", ModuleCategory.Player);
         this.setKeyCode(Keyboard.KEY_LEFT);
         this.getSettingList().addAll(Arrays.asList(
-                autoDisable, length
+                autoDisable, autoReset, length, packets
         ));
     }
 
     @Override
+    public void init() {
+        font = TTFFontRenderer.of("Roboto-Regular", 20);
+
+        super.init();
+    }
+
+    @Override
     public void onSendPacket(Events.SendPacket event) {
-        if(event.packet instanceof C03PacketPlayer) {
-            event.cancel();
+        Map<String, Boolean> packetMap = packets.getValue();
 
-            C03PacketPlayer prevPacket = null;
-            C03PacketPlayer packet = (C03PacketPlayer) event.packet;
-            if (!packetList.isEmpty()) {
-                prevPacket = packetList.get(packetList.size() - 1);
-            }
+        if(!((packetMap.get("C03Player") && event.packet instanceof C03PacketPlayer)
+                || (packetMap.get("C04Pos") && event.packet instanceof C03PacketPlayer.C04PacketPlayerPosition)
+                || (packetMap.get("C05Look") && event.packet instanceof C03PacketPlayer.C05PacketPlayerLook)
+                || (packetMap.get("C06PosLook") && event.packet instanceof C03PacketPlayer.C06PacketPlayerPosLook)
+                || (packetMap.get("C07Digging") && event.packet instanceof C07PacketPlayerDigging)
+                || (packetMap.get("C08Place") && event.packet instanceof C08PacketPlayerBlockPlacement)
+                || (packetMap.get("C09ItemChange") && event.packet instanceof C09PacketHeldItemChange)
+                || (packetMap.get("C13Abilities") && event.packet instanceof C13PacketPlayerAbilities))
+        ) return;
 
-            if (prevPacket != null && packet.isOnGround() == prevPacket.isOnGround()
-                && packet.getYaw(-1) == prevPacket.getYaw(-1)
-                && packet.getPitch(-1) == prevPacket.getPitch(-1)
-                && packet.getPositionX(-1) == prevPacket.getPositionX(-1)
-                && packet.getPositionY(-1) == prevPacket.getPositionY(-1)
-                && packet.getPositionZ(-1) == prevPacket.getPositionZ(-1))
-                return;
+        event.cancel();
 
-            packetList.add(packet);
+        Packet<?> packet = event.packet;
+        packetList.add(packet);
 
-            if (autoDisable.getValue() && length.getValue() <= packetList.size()) {
+        if (length.getValue() <= packetList.size()) {
+            if (autoDisable.getValue()) {
                 this.toggle();
+            } else if (autoReset.getValue()) {
+                sendPackets();
             }
         }
 
@@ -80,10 +102,21 @@ public class Blink extends Module {
     private void sendPackets() {
         if (!packetList.isEmpty()) {
             synchronized (packetList) {
-                for (C03PacketPlayer p : packetList) {
+                for (Packet<?> p : packetList) {
                     mc.getNetHandler().getNetworkManager().sendPacketNoEvent(p);
                 }
             }
         }
+        packetList.clear();
+    }
+
+    @Override
+    public void onRenderGui(Events.PreRenderGui event) {
+        ScaledResolution sr = new ScaledResolution(mc);
+        String text = "Length: " + packetList.size();
+
+        font.drawString(text, (sr.getScaledWidth() - font.width(text)) / 2, sr.getScaledHeight() / 1.25, 0xffffffff);
+
+        super.onRenderGui(event);
     }
 }
