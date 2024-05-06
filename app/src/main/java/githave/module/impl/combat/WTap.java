dc124556dfc21450d9f04ad472385d6ba10df419
wevez <79446154+wevez@users.noreply.github.com>
2024-05-06 21:55:53 +0900

    Update WTap.java

--- app/src/main/java/githave/module/impl/combat/WTap.java
+++ app/src/main/java/githave/module/impl/combat/WTap.java
@@ -12,30 +12,40 @@ import java.util.Arrays;
 
 public class WTap extends Module {
 
-    // TODO
-    private final ModeSetting mode = new ModeSetting.Builder("Mode", "Sprint Reset")
-            .build();
+    private final BooleanSetting sprintReset = new BooleanSetting.Builder("Sprint Reset")
+        .build();
 
-    private final DoubleSetting delay = new DoubleSetting.Builder("Delay", 100, 0, 500, 10)
-            .build();
+    private final BooleanSetting keepDistance = new BooleanSetting.Builder("Keep Distance")
+        .value(true)
+        .builder();
+
+    private final DoubleSetting keepRange = new DoubleSetting.Builder("Keep Range", 2.5, 0, 8, 0.1)
+        .visibility(keepDistance::getValue)
+        .build();
+
+    private long currentCombodelay, lastComboTime;
+
+    private boolean yeah;
 
     public WTap() {
         super("WTap", "Auto WTap", ModuleCategory.Combat);
         this.getSettingList().addAll(Arrays.asList(
-                mode,
-                delay
+            sprintReset,
+            keepDistance,
+            keepRange
         ));
     }
 
-    private final TimerUtil timer = new TimerUtil();
-    private final TimerUtil timer2 = new TimerUtil();
+    @Override
+    protected void onEnable() {
+        yeah = true;
+    }
 
     @Override
     public void onSendPacket(Events.SendPacket event) {
         if (event.packet instanceof C02PacketUseEntity) {
             C02PacketUseEntity packet = (C02PacketUseEntity)event.packet;
             if (packet.getAction() == C02PacketUseEntity.Action.ATTACK) {
-                timer2.reset();
             }
         }
         super.onSendPacket(event);
@@ -44,22 +54,47 @@ public class WTap extends Module {
     @Override
     public void onTick(Events.Tick event) {
         if (mc.thePlayer == null) return;
-        if (mc.thePlayer.hurtTime == 1) {
-            timer.reset();
+        if (KillAura.target == null) {
+            yeah = true;
+            return;
         }
-        if (KillAura.target == null) return;
         if (KillAura.target.hurtTime == 10) {
-            mc.thePlayer.reSprint = 2;
+            if (yeah) {
+                yeah = false;
+                final long currentTime = System.currentTimeMillis();
+                currentCombodelay = lastComboTime - currentTime;
+                lastComboTime = currentTime;
+            }
+        } else {
+            yeah = true;
         }
         super.onTick(event);
     }
 
     @Override
     public void onMovementInput(Events.MovementInput event) {
-        if (KillAura.target != null && mc.objectMouseOver.entityHit != null) {
-            event.input.moveForward = 0f;
-            event.input.moveStrafe = 0;
+        if (KillAura.target == null) return;
+        final int nextAttackTick = (int) (System.currentTimeMillis() - (lastComboTime + currentComboDelay) / 50;
+        if (nextAttackTick <= 0) return;
+        final int tikcs = 10;
+        final List<Vec3> positions = PlayerUtil.predict(ticks);
+        final List<Vec3> enemyPositions = PlayerUtil.predict(KillAura.target, ticks);
+        boolean shouldStop = false;
+        for (int i = 0; i < ticks; i++) {
+            final Vec3 p = positions.get(i), e = enemyPositions.get(i);
+            final boolean shouldInRange = i >= nextAttackTick;
+            if (shouldInRange) {
+                if (p.distanceTo(e) > this.keepRange.getValue()) {
+                    shouldStop = false;
+                    break;
+                }
+            } else {
+                if (p.distanceTo(e) < this.keepRange.getValue()) {
+                    shouldStop = true;
+                }
+            }
         }
+        if (shouldStop) event.input.moveForward = 0f;
         super.onMovementInput(event);
     }
 }
